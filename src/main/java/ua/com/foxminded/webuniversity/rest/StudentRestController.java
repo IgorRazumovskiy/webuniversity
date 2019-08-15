20efda0ee60b6773281a16ec63b728eb8a162523
igor <igor@DESKTOP-55M0B0S>
2019-08-15 17:09:35 +0300

    commit Swagger documentation support 1

--- src/main/java/ua/com/foxminded/webuniversity/rest/StudentRestController.java
+++ src/main/java/ua/com/foxminded/webuniversity/rest/StudentRestController.java
@@ -15,9 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import ua.com.foxminded.webuniversity.entity.Student;
 import ua.com.foxminded.webuniversity.service.StudentService;
 
+@Api(value = "CRUD student", description = "student endpoints")
 @RestController
 @RequestMapping("/students")
 public class StudentRestController {
@@ -28,26 +31,31 @@ public class StudentRestController {
         this.studentService = studentService;
     }
 
+    @ApiOperation(value = "get all students")
     @GetMapping()
     public ResponseEntity<List<Student>> findAll() {
         return ResponseEntity.ok(studentService.findAll());
     }
 
+    @ApiOperation(value = "find student by id")
     @GetMapping("{studentId}")
     public ResponseEntity<Student> findOne(@PathVariable Integer studentId) {
         return ResponseEntity.ok(studentService.findOne(studentId));
     }
 
+    @ApiOperation(value = "add new student")
     @PostMapping()
     public ResponseEntity<Student> create(@Valid @RequestBody Student student) {
         return new ResponseEntity<Student>(studentService.create(student), HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "update student")
     @PutMapping()
     public ResponseEntity<Student> update(@Valid @RequestBody Student student) {
         return ResponseEntity.ok(studentService.update(student));
     }
 
+    @ApiOperation(value = "delete student by id")
     @DeleteMapping("{studentId}")
     public ResponseEntity<String> delete(@PathVariable Integer studentId) {
         studentService.delete(studentId);
